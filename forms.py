2e1037caada1190e04f8917bc7183ad01945dc1a
dangtruong06 <DANGTT066@GMAIL.COM>
2026-06-21 13:54:08 -0700

    add auth templates: login, register, and base layout enabling registration and user log ins.

--- forms.py
+++ forms.py
@@ -7,10 +7,8 @@ class Register(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     name = StringField('Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register Now!')
 
 # LOGIN FORM
 class Login(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])   
-    submit = SubmitField('Log In')
